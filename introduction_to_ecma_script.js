25a8e63f7166ab8d0fabf15e2ee1fa975e5a8221
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-05-21 08:38:50 +0100

    file deletions

--- introduction_to_ecma_script.js
+++
@@ -1,2 +0,0 @@
-//EcmaScript is next generation JavaScript
-// Most browsers cannot support es6 so compilers like Babel compiles es6 to es5 for browsers to understand.
--- let_keyword.js
+++
@@ -1,2 +0,0 @@
-let age = 30;
-console.log(age)
