b59cc2964465c889df03bdce5bbf24caa798dad5
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-05-21 13:00:23 +0100

    chapter2

---
+++ letconstvar.js
@@ -0,0 +1,10 @@
+// const TAX_RATE = 0.08;
+
+// function calculateFInalAmmount(amount){
+//     amount = amount + (amount * TAX_RATE)
+//     return amount
+// }
+
+// console.log(calculateFInalAmmount(
+
+// ))
